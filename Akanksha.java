dddd74b33e871f5d0aa61f6bb8eae5738acb0bc3
AkankshDeshmukh <akankshadeshmukh668@gmail.com>
2025-11-19 23:37:32 +0530

    Akanksha

---
+++ Akanksha.java
@@ -0,0 +1,15 @@
+class Akanksha{
+
+        
+   public static void main(String[]args)
+   {
+       int a=20;
+       System.out.println("Akanksha");
+       System.out.println("Deshmukh");
+       System.out.println("Address:pune ,akurdi");
+       System.out.println("value of a :" +a);
+   }
+
+
+
+}
